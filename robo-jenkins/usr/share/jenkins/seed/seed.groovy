23aacb4faccacab5f3992caa9695253a0002006c
Jordan Cohen <hypergig@gmail.com>
2016-11-17 10:58:41 -0500

    basic frame work for creating jobs, just docker build as of now

--- robo-jenkins/usr/share/jenkins/seed/seed.groovy
+++ robo-jenkins/usr/share/jenkins/seed/seed.groovy
@@ -1,10 +1,59 @@
 // the seed job dsl script
 
-job('nginx-test') {
-    scm {
-        git('https://github.com/nginxinc/docker-nginx')
+
+File file = new File("${WORKSPACE}/repo_list")
+file.readLines().each { repo ->
+    println "Processing repository: $repo"
+    def (org, name) = repo.split('/')[-2,-1]
+    String parent_folder = "$org-$name"
+
+    folder(parent_folder) {
+        //displayName('JOB!')
+        description("Build jobs for $repo")
     }
-    steps {
-        shell('set -e; env | sort; docker build stable/alpine/.')
+
+    println "fetching branches"
+    def process = "git ls-remote -h $repo".execute()
+    process.waitForOrKill(5000)
+    
+    def stdout = new StringBuilder(), stderr = new StringBuilder()
+    process.waitForProcessOutput(stdout, stderr)
+    
+    int rc = process.exitValue()
+    if (rc) {
+      println "failed to fetch branches:\n$stderr"
+      return false
+    }
+
+    stdout.eachLine { line ->
+        def branch = line.split('/')[-1]
+        println "Processing branch: $branch"
+
+        println "Creating job: $parent_folder/$branch"
+        job("$parent_folder/$branch") {
+            scm {
+                git(repo, branch)
+            }
+            steps {
+                // todo - modularize and template build steps 
+                shell("""
+                        bash -c '
+                            set -e
+                            echo \"---| environment variables |---\"
+                            env | sort
+                            contexts=\"\$(find . -type f -name Dockerfile -exec dirname {} \\;)\"
+                            echo \"---| docker contexts |---\"
+                            echo \"\$contexts\"
+                            while read context
+                              do
+                                echo \"---| building docker context \$context |---\"
+                                docker build \"\$context\" && {
+                                    echo \"---| successfully built context \$context |---\"; } || {
+                                    echo \"---| failed to build context \$context |---\"; }
+                            done <<< \"\$contexts\"
+                        '
+                    """)
+            }
+        }
     }
 }
